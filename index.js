674657cc50a8ba9e48571e3cccfee0ea07333f54
azl397985856 <695694307@qq.com>
2016-12-12 14:22:26 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function writer(fileName, content){
     var a = document.createElement('a');
 	var url = window.URL.createObjectURL(new Blob([content]));
-	var filename = fileName;
 	a.href = url;
 	a.download = filename;
 	a.click();
