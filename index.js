299e852834af2ec518524b28de75dc66f4b4869a
azl397985856 <695694307@qq.com>
2017-01-12 15:46:39 +0800

    Mime type added. encoding filed added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function writer(fileName, content){
+function writer(fileName, content, encoding){
     var a = document.createElement('a');
-	var url = window.URL.createObjectURL(new Blob([content]));
+	var url = window.URL.createObjectURL(new Blob([content], { type: "text/plain;charset=" + encoding }));
 	a.href = url;
-	a.download = filename;
+	a.download = fileName;
 	a.click();
 	window.URL.revokeObjectURL(url);
 }
