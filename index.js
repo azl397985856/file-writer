a5be0604ff9d8c0b1e3c71c3ad805314576c034f
azl397985856 <695694307@qq.com>
2017-02-15 16:13:20 +0800

    mime type supported

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function writer(fileName, content, encoding){
+function writer(fileName, content, option){
     var a = document.createElement('a');
-	var url = window.URL.createObjectURL(new Blob([content], { type: "text/plain;charset=" + (encoding || 'utf-8') }));
+	var url = window.URL.createObjectURL(new Blob([content],
+		{ type: (option.type || "text/plain") + ";charset=" + (option.encoding || 'utf-8') }));
 	a.href = url;
 	a.download = fileName || 'file';
 	a.click();
